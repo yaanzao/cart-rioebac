e5882f3843d57159dbb532111be0982bb0d4999a
yaanzao <86253382+yaanzao@users.noreply.github.com>
2024-04-01 21:16:50 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usuário

--- PROJETINHO.cpp
+++ PROJETINHO.cpp
@@ -134,6 +134,7 @@ int main ()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n");
+	printf("\t4 - Sair do sistema\n");
 	printf("Opção: "); // fim do menu
 	
 	scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -153,6 +154,11 @@ int main ()
 				
 				case 3:
 				deletar();
+				break;
+				
+				case 4:
+				printf("Obrigado por utilizar nosso sistema!\n");
+				return 0;
 				break;
 				
 				default:	
